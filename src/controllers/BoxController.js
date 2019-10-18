42b2fb88b4ec1c5cba6223d090d6aedf7d92a359
Juliano <julianotrabalho2016@gmail.com>
2019-10-18 13:34:57 -0300

    UPDATE - upload files

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -5,6 +5,13 @@ class BoxController{
     const box = await Box.create(req.body)
     return res.json(box);
   }
+  async show(req, res){
+    const box = await Box.findById(req.params.id).populate({
+      path: 'files',
+      options: {sort: {createdAt: -1}}
+    })
+    return res.json(box);
+  }
 }
 
 module.exports = new BoxController();
