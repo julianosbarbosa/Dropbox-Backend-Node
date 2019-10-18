e0bb598a5dcb595954d5fd91e2c4eec906ee131d
Juliano <julianotrabalho2016@gmail.com>
2019-10-18 11:36:57 -0300

    UPDATE - Create boxes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const routes = express.Router();
+const BoxController = require('./controllers/BoxController')
 
-routes.get('/teste', (req, res)=>{
-  return  res.send('Hello World!');
-})
+routes.post("/boxes", BoxController.store)
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express()
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-ixcod.mongodb.net/oministack?retryWrites=true&w=majority',
+{ useNewUrlParser: true }
+)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
